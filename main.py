6e5b8bc87aaa2320946977b7af9d56b72c43769e
beastr45 <bearweston07@gmail.com>
2023-04-30 14:22:28 -0500

    Polished Swarm class removed uneccesary files not commited

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 #Made in 2023 by Bear BlinSchauer
 #see license for more info
+import random
 import pygame
 from pygame import mixer
 
@@ -15,6 +16,7 @@ dt = 0
 #LOAD IMAGES
 background_image = pygame.image.load("assets/img/background.jpg")
 background_image = pygame.transform.smoothscale(background_image, screen.get_size())
+# explosion_image = pygame.image.load("assets/img/explosion.gif")
 #list of possible enemy images
 enemy_image1 = pygame.image.load("assets/img/enemy.png")
 enemy_image1 = pygame.transform.scale(enemy_image1, (100, 100))
@@ -63,9 +65,7 @@ class new_player:
         screen.blit(self.player_img,(self.player_pos.x-self.player_img.get_width()/2,self.player_pos.y - self.player_img.get_height()/2))
 #
 
-#True for left to right false for right to left
-enemy_direction = False
-#TODO FIX ENEMY UNALIGNMENT OVER TIME ISSUE
+#self.direction True for left to right false for right to left
 class new_enemy:
     def __init__(self, origin,img_source):
         #images
@@ -73,24 +73,31 @@ class new_enemy:
         self.origin = origin
         self.enemy_pos = pygame.Vector2(origin)
         self.direction = True
+        self.enemy_hitbox = pygame.Rect(self.origin[0],self.origin[1], self.enemy_image.get_width(),self.enemy_image.get_height())
     def switch_direction(self):
         self.direction = not self.direction
     def update(self):
-        global enemy_direction
         if self.direction:
             self.enemy_pos.x += 100 * dt
+            self.enemy_hitbox = (self.enemy_pos.x,self.enemy_pos.y, self.enemy_image.get_width(),self.enemy_image.get_height())
         elif not self.direction:
             self.enemy_pos.x -= 100 * dt
-        if self.enemy_pos.x < 0 or self.enemy_pos.x+self.enemy_image.get_width() > screen.get_width():
-            enemy_direction = not enemy_direction
+            self.enemy_hitbox = (self.enemy_pos.x,self.enemy_pos.y, self.enemy_image.get_width(),self.enemy_image.get_height())
+        # if self.enemy_pos.x < 0 or self.enemy_pos.x+self.enemy_image.get_width() > screen.get_width():
+        #     enemy_direction = not enemy_direction
 
         #check edge collision
         #clamp the x values to the edges of the screen
         self.enemy_pos.x = max(min(screen.get_width(), self.enemy_pos.x), 0)
+    def enemy_attack(self):
+            lasers.append(new_laser(self.enemy_pos, False))
 
     def draw(self):
-        pygame.draw.circle(screen, "red", self.enemy_pos, 10)
         screen.blit (self.enemy_image,self.enemy_pos)
+        pygame.draw.rect(screen,"blue", self.enemy_hitbox, 10)
+    def __del__(self):
+        print("enemy object deleted")
+        print("explosion animation not yet added")
 #
 class new_enemy_swarm:
     def __init__(self,row_count,columm_count,swarm_image):
@@ -101,22 +108,60 @@ class new_enemy_swarm:
         self.enemy_row_count = row_count
         self.enemy_columm_count = columm_count
         self.enemies = []
+        self.enemy_row_list =[]
+        #construct a list of enemies based of the columms and rows passed
         for i in range (1,self.enemy_row_count+1):
             #divide screen width by the number of iteratiosn in the loop
             for f in range (1, self.enemy_columm_count+1):
-                self.enemies.append(new_enemy((((screen.get_width()-screen.get_width()/self.enemy_columm_count)/self.enemy_columm_count*(f))-(screen.get_width()/self.enemy_columm_count),i*self.row_size-self.row_size),swarm_image))
-        self.bounds = [pygame.Rect(0,0, 50,100),pygame.Rect(((screen.get_width()-screen.get_width()/self.enemy_columm_count)/self.enemy_columm_count*(self.enemy_row_count)),0, 50,100)]
+                self.enemy_row_list.append(new_enemy((((screen.get_width()-screen.get_width()/self.enemy_columm_count)/self.enemy_columm_count*(f))-(screen.get_width()/self.enemy_columm_count),i*self.row_size-self.row_size),swarm_image))
+            self.enemies.append(self.enemy_row_list)
+        self.rect_size = (self.enemies[0][0].enemy_image.get_width(),self.enemies[0][0].enemy_image.get_height()*self.enemy_row_count)
+        self.bounds = [pygame.Rect(self.enemies[0][0].enemy_pos.x,self.enemies[0][0].enemy_pos.y,self.rect_size[0],self.rect_size[1]),pygame.Rect(self.enemies[0][self.enemy_columm_count-1].enemy_pos.x,self.enemies[0][self.enemy_columm_count-1].enemy_pos.y,self.rect_size[0],self.rect_size[1])]
+
+
+    def check_laser_collisions(self):
+        for rows in self.enemies:
+            for columm in rows:
+                for laser in lasers:
+                    if laser.laser_hitbox.colliderect(columms.enemy_hitbox) and laser.direction == True:
+                        self.enemies[rows].remove(columm)
+                        lasers.remove(laser)
     def update(self):
-        # for i in self.enemies:
-        #      i.update()
-        if self.bounds[1].collidepoint(0,0) or self.bounds[1].collidepoint(screen.get_width(),0):
+        #roll dice to make all enemies attack at the same time
+        # if random.randint(1,100) == 5:
+        #     for i in self.enemies:
+        #         lasers.append(new_laser(i.enemy_pos, False))
+
+        #update the hitboxes for the edges based on the list
+        #TODO FIX HITBOXES BREAKING AFTER LIST IS CHANGED
+        #perhaps use matrices to store enemies
+        self.bounds = [pygame.Rect(self.enemies[0][0].enemy_pos.x,self.enemies[0][0].enemy_pos.y,self.rect_size[0],self.rect_size[1]),pygame.Rect(self.enemies[0][self.enemy_columm_count-1].enemy_pos.x,self.enemies[0][self.enemy_columm_count-1].enemy_pos.y,self.rect_size[0],self.rect_size[1])]
+        # self.bounds = [self.bounds[0].move(10*dt,0),self.bounds[1].move(10*dt,0)]
+
+        #detect if the side hitboxes touch the wall and update evey enemy in the enemies list
+        if self.bounds[0].collidepoint(0,0) or self.bounds[1].collidepoint(screen.get_width(),0):
             for i in self.enemies:
-                i.switch_direction()
-        self.bounds[0] = self.bounds[0].move(100*dt,0)
+                for f in i:
+                    f.switch_direction()
+
+        #call all of the update fuctions from within the enemy objects
+        for i in self.enemies:
+            for f in i:
+                f.update()
+
+        #run a for loop to check the hitboxes of enemies with lasers
+        #enemies are deleted if they come in contact with laser
+        self.check_laser_collisions()
+
+    #draw function - draws all enemies in the swarm
     def draw(self):
         for i in self.enemies:
-            i.draw()
+            for f in i:
+                f.draw()
+
+        #draws left and right hitboxes for the swarm
         pygame.draw.rect(screen, "red",self.bounds[1],5)
+        pygame.draw.rect(screen, "red",self.bounds[0],5)
 #
 
 lasers = []
@@ -134,13 +179,15 @@ class new_laser:
         # self.laser_pos = pygame.Vector2(0,screen.get_height()-laser_height)
         self.laser_pos = pygame.Vector2(origin)
         laser_sound.play()
+        self.laser_hitbox = pygame.Rect(self.laser_pos.x, self.laser_pos.y,laser_width,laser_height)
     def update(self):
-        if dir:
+        if self.direction:
             self.laser_pos.y -= laser_speed *dt
-        if not dir:
+        if not self.direction:
             self.laser_pos.y += laser_speed *dt
+        self.laser_hitbox = pygame.Rect(self.laser_pos.x, self.laser_pos.y,laser_width,laser_height)
     def draw(self):
-        pygame.draw.rect(screen, "red",(self.laser_pos.x,self.laser_pos.y,laser_width ,laser_height),0)
+        pygame.draw.rect(screen, "red",self.laser_hitbox,0)
 #
 def handle_lasers():
     for i in lasers:
@@ -150,13 +197,11 @@ def handle_lasers():
             lasers.remove(i)
 #
 
-enemies = []
 def startup():
     global player
     global enemy_swarm
     player = new_player(laser_interval)
-    enemy_swarm = new_enemy_swarm(6,10,enemy_images[0])
-
+    enemy_swarm = new_enemy_swarm(2,10,enemy_images[0])
 #
 def cleanup():
     pass
@@ -182,6 +227,8 @@ def render():
 
     #draw enemies
     enemy_swarm.draw()
+
+    # screen.blit(explosion_image,(0,0))
 
     # flip() the display to put your work on screen
     pygame.display.flip()
